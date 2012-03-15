1de8c621af6bfc27b1c8e8ba871139ea0f6195df
cristian64@gmail.com <cristian64@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-15 20:37:48 +0000

    Added a platform. Added a Conversion class to transform from world units to display units.

---
+++ Nobots/Nobots/Nobots/Conversion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Nobots
+{
+    public class Conversion
+    {
+        public static float DisplayUnitsToWorldUnitsRatio = 100.0f;
+        public static float WorldUnitsToDisplayUnitsRatio = 1 / DisplayUnitsToWorldUnitsRatio;
+
+        public static float ToDisplay(float u)
+        {
+            return u * DisplayUnitsToWorldUnitsRatio;
+        }
+
+        public static float ToWorld(float u)
+        {
+            return u * WorldUnitsToDisplayUnitsRatio;
+        }
+
+        public static Vector2 ToDisplay(Vector2 u)
+        {
+            return u * DisplayUnitsToWorldUnitsRatio;
+        }
+
+        public static Vector2 ToWorld(Vector2 u)
+        {
+            return u * WorldUnitsToDisplayUnitsRatio;
+        }
+    }
+}
