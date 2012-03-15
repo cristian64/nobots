6b44bc2332f7f8210e9cb054e0bdb4ea8a911d10
cristian64@gmail.com <cristian64@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-15 17:46:16 +0000

    Added Farseer Physics Engine. Added a Camera class almost empty and Scene class empty.

---
+++ Nobots/Nobots/Nobots/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Nobots
+{
+    public class Camera : GameComponent
+    {
+        public Vector2 Position;
+
+        public Camera(Game game)
+            : base(game)
+        {
+            Position = Vector2.Zero;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ Nobots/Nobots/Nobots/Scene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Nobots
+{
+    public class Scene : DrawableGameComponent
+    {
+        public Scene(Game game)
+            : base(game)
+        {
+        }
+    }
+}
