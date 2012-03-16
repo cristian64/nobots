ea0274d7a84daa20549732bccbe78c9521f693cf
cristian64@gmail.com <cristian64@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-16 09:04:09 +0000

    Camera moves smoothly following the target.

--- trunk/Nobots/Nobots/Nobots/Camera.cs
+++ trunk/Nobots/Nobots/Nobots/Camera.cs
@@ -8,23 +8,40 @@ namespace Nobots
 {
     public class Camera : DrawableGameComponent
     {
+        public float Margin;
+        public float Speed;
         public Element Target;
         public Vector2 Position;
-        public float Zoom;
 
         public Camera(Game game)
             : base(game)
         {
             Position = Vector2.Zero;
-            Zoom = 100;
+            Speed = Conversion.ToWorld(10);
+            Margin = Conversion.ToWorld(50);
         }
 
         public override void Update(GameTime gameTime)
         {
             if (Target != null)
             {
-                Position = Target.Position;
-                Position -= new Vector2(Conversion.ToWorld(GraphicsDevice.PresentationParameters.BackBufferWidth / 2), Conversion.ToWorld(GraphicsDevice.PresentationParameters.BackBufferHeight / 2));
+                Vector2 centeredPosition = Target.Position - new Vector2(Conversion.ToWorld(GraphicsDevice.PresentationParameters.BackBufferWidth / 2), Conversion.ToWorld(GraphicsDevice.PresentationParameters.BackBufferHeight / 2));
+                float distance = Vector2.Distance(centeredPosition, Position);
+                if (distance > Margin)
+                {
+                    Position = centeredPosition + Vector2.Normalize(Position - centeredPosition) * Margin;
+                }
+                else
+                {
+                    if (distance < Speed * (float)gameTime.ElapsedGameTime.TotalSeconds)
+                    {
+                        Position = centeredPosition;
+                    }
+                    else
+                    {
+                        Position += Vector2.Normalize(centeredPosition - Position) * Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    }
+                }
             }
             base.Update(gameTime);
         }
