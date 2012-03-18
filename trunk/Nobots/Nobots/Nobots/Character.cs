47ab98c82589f3e8c35603f09f52e8b0822832bd
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-18 16:12:51 +0000

    Added body to the whole character.

--- trunk/Nobots/Nobots/Nobots/Character.cs
+++ trunk/Nobots/Nobots/Nobots/Character.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
+using FarseerPhysics.Dynamics.Joints;
 using Microsoft.Xna.Framework.Input;
 
 namespace Nobots
@@ -13,7 +14,9 @@ namespace Nobots
     public class Character : Element
     {
         Body body;
+        Body torso;
         Texture2D texture;
+        RevoluteJoint revoluteJoint;
         public SpriteEffects Effect;
 
         public override Vector2 Position
@@ -36,11 +39,26 @@ namespace Nobots
         protected override void LoadContent()
         {
             texture = Game.Content.Load<Texture2D>("girl");
+
+            torso = BodyFactory.CreateRectangle(scene.World, Conversion.ToWorld(texture.Width/2), Conversion.ToWorld(texture.Height - texture.Width/8),
+                1.0f);
             body = BodyFactory.CreateCircle(scene.World, Conversion.ToWorld(texture.Width / 4.0f), 1.0f);
+
             body.Position = new Vector2(2.812996f, 2.083698f);
+            torso.Position = new Vector2(body.Position.X - Conversion.ToWorld(texture.Width / 2),
+                body.Position.Y + Conversion.ToWorld(texture.Width / 4 - texture.Height));
+
+            torso.BodyType = BodyType.Dynamic;
+            torso.Mass = 0;
+            torso.FixedRotation = true;
+
             body.BodyType = BodyType.Dynamic;
             body.Friction = 1000.0f;
 
+            revoluteJoint = new RevoluteJoint(torso, body, Conversion.ToWorld(new Vector2(texture.Width/12, 
+                texture.Height/(2.2f))), Vector2.Zero);
+           scene.World.AddJoint(revoluteJoint);
+           
             base.LoadContent();
         }
 
@@ -54,7 +72,7 @@ namespace Nobots
         {
             scene.SpriteBatch.Begin();
             //scene.SpriteBatch.Draw(texture, Conversion.ToDisplay(body.Position - scene.Camera.Position), null, Color.White, 0.0f, new Vector2(texture.Width / 2, texture.Height / 2), 1.0f, Effect, 0);
-            scene.SpriteBatch.Draw(texture, Conversion.ToDisplay(body.Position - scene.Camera.Position), null, Color.White, 0.0f, new Vector2(texture.Width / 2, texture.Height - texture.Width / 4), 1.0f, Effect, 0);
+            scene.SpriteBatch.Draw(texture, Conversion.ToDisplay(torso.Position - scene.Camera.Position), null, Color.White, 0.0f, new Vector2(texture.Width / 2, texture.Height * 5/11), 1.0f, Effect, 0);
             scene.SpriteBatch.End();
 
             base.Draw(gameTime);
@@ -84,7 +102,8 @@ namespace Nobots
 
             if (keybState.IsKeyDown(Keys.Up) && previousState.IsKeyUp(Keys.Up))
             {
-                body.ApplyForce(new Vector2(0, -8));
+                body.ApplyForce(new Vector2(0, -75));
+              //  torso.ApplyForce(new Vector2(0, -60));
             }
 
             previousState = keybState;
