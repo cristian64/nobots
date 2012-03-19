da4cf755df9f76b696c0523135709ae9732bc0ba
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-19 17:56:04 +0000

    Added states and girl_moving.png.

---
+++ trunk/Nobots/Nobots/Nobots/IdleCharacterState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Nobots
+{
+    public class IdleCharacterState : CharacterState
+    {
+        public IdleCharacterState(Scene scene, Character character) 
+            : base(scene, character)
+        {
+            texture = scene.Game.Content.Load<Texture2D>("girl");
+            character.texture = texture;
+            characterWidth = texture.Width;
+            characterHeight = texture.Height;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
---
+++ trunk/Nobots/Nobots/Nobots/RunningCharacterState.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Nobots
+{
+    public class RunningCharacterState : CharacterState
+    {
+
+        public RunningCharacterState(Scene scene, Character character)
+            : base(scene, character)
+        {
+            texture = scene.Game.Content.Load<Texture2D>("girl_moving");
+            characterWidth = texture.Width/8;
+            characterHeight = texture.Height/5;
+            character.texture = texture;
+            textureXmin = 0;
+            textureYmin = 0;
+            Console.WriteLine(textureXmin + ", " + textureYmin);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            changeRunningTextures();
+        }
+
+        private Vector2 changeRunningTextures()
+        {
+            if (textureXmin == (texture.Width/8)*5 && textureYmin == (texture.Height/5)*4)
+            {
+                textureXmin = 0;
+                textureYmin = 0;
+            }
+            else if (textureXmin == texture.Width)
+            {
+                textureXmin = 0;
+                textureYmin += texture.Height / 5;
+            }
+            else
+            {
+                textureXmin += texture.Width / 8;
+               // textureYmin += texture.Height / 5;
+            }
+            Console.WriteLine("X: " + textureXmin + ", Y: " + textureYmin + ", Width: " + characterWidth +
+                ", Height: " + characterHeight);
+
+            return new Vector2(textureXmin, textureYmin);
+        }
+    }
+}
