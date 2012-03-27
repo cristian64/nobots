cddbb9c64e4fbe2741fdb351ef0fe145cfa6b77f
cristian64@gmail.com <cristian64@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-27 22:50:09 +0000

    Effect for the character to become a yellow energy.

--- trunk/Nobots/Nobots/Nobots/Energy.cs
+++ trunk/Nobots/Nobots/Nobots/Energy.cs
@@ -14,10 +14,17 @@ namespace Nobots
 {
     class Energy : Character
     {
+        Effect effect;
 
         public Energy(Game game, Scene scene)
             : base(game, scene)
         {
+            body.Position = new Vector2(32.60955f, -2.803332f);
+
+            body.CollisionCategories = ElementCategory.ENERGY;
+            torso.CollisionCategories = ElementCategory.ENERGY;
+
+            effect = Game.Content.Load<Effect>("energy");
         }
 
         protected override void UpActionStart()
@@ -42,6 +49,15 @@ namespace Nobots
 
         protected override void BActionStop()
         {
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            scene.SpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, null, null, null, effect);
+            scene.SpriteBatch.Draw(texture, Conversion.ToDisplay(Position - scene.Camera.Position),
+                new Rectangle(State.textureXmin, State.textureYmin, State.characterWidth, State.characterHeight),
+                Color.White, 0.0f, new Vector2(State.characterWidth / 2, State.characterHeight / 2), 1.0f, Effect, 0);
+            scene.SpriteBatch.End();
         }
     }
 }
