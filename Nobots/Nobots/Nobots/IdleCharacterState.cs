6e66ddd1edcdf85ba0ced407f1f71520c66964e8
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-03-27 16:24:25 +0000

    Now the only texture used is girl_moving.png

--- Nobots/Nobots/Nobots/IdleCharacterState.cs
+++ Nobots/Nobots/Nobots/IdleCharacterState.cs
@@ -12,10 +12,10 @@ namespace Nobots
         public IdleCharacterState(Scene scene, Character character) 
             : base(scene, character)
         {
-            texture = scene.Game.Content.Load<Texture2D>("girl");
+            texture = scene.Game.Content.Load<Texture2D>("girl_moving");
             character.texture = texture;
-            characterWidth = texture.Width;
-            characterHeight = texture.Height;
+            characterWidth = texture.Width / 8;
+            characterHeight = texture.Height / 5;
             textureXmin = 0;
             textureYmin = 0;
         }
