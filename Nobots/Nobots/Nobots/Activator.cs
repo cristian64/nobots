a5db6930b1f0710e51c47dc697ae1af06e5c354e
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-04-12 19:20:03 +0000

    Changed Activator.cs to activate elements by Id.

--- Nobots/Nobots/Nobots/Activator.cs
+++ Nobots/Nobots/Nobots/Activator.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using System.Diagnostics;
 
 namespace Nobots
 {
     public class Activator : Element
     {
-        public IActivable activableElement;
+        protected IActivable activableElement;
+        public IActivable ActivableElement
+        {
+            get 
+            {
+                if (activableElement == null)
+                    foreach (Element e in scene.Elements)
+                    {
+                        if (e as IActivable != null && e.Id == activableElementId)
+                        {
+                            activableElement = (IActivable)e;
+                            break;
+                        }
+                    }
+                Debug.Assert(activableElement == null, "The element " + Id + " looked for " + activableElementId + " but that ID wasn't in the list.");
+                return activableElement; 
+            }
+        }
+
+        protected String activableElementId;
+        public String ActivableElementId
+        {
+            get { return activableElementId; }
+            set
+            {
+                activableElementId = value;
+            }
+        }
 
         public Activator(Game game, Scene scene) : base(game, scene)
         {
