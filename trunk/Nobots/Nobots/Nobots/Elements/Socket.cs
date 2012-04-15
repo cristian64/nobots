7e81a49c8e5037b4723743b7178493512302fe64
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-04-15 23:09:07 +0000

    Enabled rotation of sockets.

--- trunk/Nobots/Nobots/Nobots/Elements/Socket.cs
+++ trunk/Nobots/Nobots/Nobots/Elements/Socket.cs
@@ -57,7 +57,10 @@ namespace Nobots.Elements
         public override float Rotation
         {
             get { return 0; }
-            set { }
+            set 
+            {
+                body.Rotation = value;
+            }
         }
 
         public override float Width
@@ -95,7 +98,7 @@ namespace Nobots.Elements
         public override void Draw(GameTime gameTime)
         {
             scene.SpriteBatch.Begin();
-            scene.SpriteBatch.Draw(texture, scene.Camera.Scale * Conversion.ToDisplay(body.Position - scene.Camera.Position), null, Color.White, 0, new Vector2(texture.Width / 2, texture.Height / 2), scene.Camera.Scale, SpriteEffects.None, 0);
+            scene.SpriteBatch.Draw(texture, scene.Camera.Scale * Conversion.ToDisplay(body.Position - scene.Camera.Position), null, Color.White, body.Rotation, new Vector2(texture.Width / 2, texture.Height / 2), scene.Camera.Scale, SpriteEffects.None, 0);
             scene.SpriteBatch.End();
             base.Draw(gameTime);
         }
