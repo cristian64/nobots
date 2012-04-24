49697059d25351fcfe8c8702eb5b7500504aedd7
cristian64@gmail.com <cristian64@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-04-24 17:53:59 +0000

    Added a few classes for sounds and transitions.

---
+++ trunk/Nobots/Nobots/Nobots/Elements/Sound.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using IrrKlang;
+
+namespace Nobots.Elements
+{
+    public class Sound : Element
+    {
+        private float volume = 1;
+
+        public float Volume
+        {
+            get { return volume; }
+            set
+            {
+                volume = value;
+                if (sound != null)
+                    sound.Volume = 1;
+            }
+        }
+
+        public override float Width
+        {
+            get { return 0.1f; }
+            set { }
+        }
+
+        public override float Height
+        {
+            get { return 0.1f; }
+            set { }
+        }
+
+        Vector2 position;
+        public override Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+                createSound();
+            }
+        }
+
+        public override float Rotation
+        {
+            get
+            {
+                return MathHelper.PiOver4;
+            }
+            set
+            {
+            }
+        }
+
+        private String soundName = "";
+        public String SoundName
+        {
+            get { return soundName; }
+            set
+            {
+                soundName = value;
+                createSound();
+            }
+        }
+
+        ISound sound = null;
+
+        public Sound(Game game, Scene scene, Vector2 position)
+            : base(game, scene)
+        {
+            this.position = position;
+        }
+
+        private void createSound()
+        {
+            if (sound != null)
+                sound.Dispose();
+            sound = scene.ISoundEngine.Play3D(@"Content\sounds\" + soundName, position.X, position.Y, 0);
+            if (sound != null)
+            {
+                sound.Volume = volume;
+            }
+        }
+    }
+}
