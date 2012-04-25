88e921a93366afeb1681a4adaac6c15a5c3ceae7
cristian64@gmail.com <cristian64@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-04-25 21:17:46 +0000

    Fade in and fade out for the ambience sound. There is still no transition sounds.

--- Nobots/Nobots/Nobots/AmbienceSound.cs
+++ Nobots/Nobots/Nobots/AmbienceSound.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using IrrKlang;
+using Nobots.Elements;
 
 namespace Nobots
 {
@@ -12,6 +13,11 @@ namespace Nobots
         private Scene scene;
         private ISound ambienceLabNormal;
         private ISound ambienceLabEnergy;
+        private Element previous;
+        private float fadeSpeed = 1;
+        private bool inTransitionToEnergy = false;
+        private bool inTransitionToNormal = false;
+
 
         public AmbienceSound(Game game, Scene scene)
             : base(game)
@@ -19,13 +25,57 @@ namespace Nobots
             this.scene = scene;
 
             ambienceLabNormal = scene.ISoundEngine.Play2D("Content\\sounds\\music\\ambiencelabnormal.mp3");
+            ambienceLabNormal.Looped = true;
+
             ambienceLabEnergy = scene.ISoundEngine.Play2D("Content\\sounds\\music\\ambiencelabenergy.mp3");
+            ambienceLabEnergy.Looped = true;
             ambienceLabEnergy.Paused = true;
+            ambienceLabEnergy.Volume = 0;
         }
 
         public override void Update(GameTime gameTime)
         {
+            // Check when the player change the state.
+            if (scene.InputManager.Character is Energy && !(previous is Energy))
+            {
+                inTransitionToEnergy = true;
+                inTransitionToNormal = false;
+                Console.WriteLine("Normal -> Energy");
+            }
+            else if (!(scene.InputManager.Character is Energy) && previous is Energy)
+            {
+                inTransitionToEnergy = false;
+                inTransitionToNormal = true;
+                Console.WriteLine("Energy -> Normal");
+            }
+            previous = scene.InputManager.Character;
 
+            // Process sound effect according to the transitions.
+            if (inTransitionToEnergy)
+            {
+                ambienceLabEnergy.Paused = false;
+                ambienceLabEnergy.Volume = Math.Min(1, ambienceLabEnergy.Volume + fadeSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                ambienceLabNormal.Volume = Math.Max(0, ambienceLabNormal.Volume - fadeSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+                if (ambienceLabNormal.Volume == 0)
+                {
+                    ambienceLabNormal.Paused = true;
+                    inTransitionToEnergy = false;
+                }
+            }
+
+            if (inTransitionToNormal)
+            {
+                ambienceLabNormal.Paused = false;
+                ambienceLabEnergy.Volume = Math.Max(0, ambienceLabEnergy.Volume - fadeSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                ambienceLabNormal.Volume = Math.Min(1, ambienceLabNormal.Volume + fadeSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+                if (ambienceLabEnergy.Volume == 0)
+                {
+                    ambienceLabEnergy.Paused = true;
+                    inTransitionToNormal = false;
+                }
+            }
         }
     }
 }
