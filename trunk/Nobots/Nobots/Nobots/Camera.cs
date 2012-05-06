870bdafc2c0cc54e876374a4edd1bf246fa6edca
txemavr@gmail.com <txemavr@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-05-06 21:17:42 +0000

    *removed a little bug

--- trunk/Nobots/Nobots/Nobots/Camera.cs
+++ trunk/Nobots/Nobots/Nobots/Camera.cs
@@ -108,7 +108,7 @@ namespace Nobots
                 ListenerPosition = Target.Position;
             else
                 ListenerPosition = Position + new Vector2(Conversion.ToWorld(GraphicsDevice.Viewport.Width / 2 / Scale), Conversion.ToWorld(GraphicsDevice.Viewport.Height / 1.5f / Scale));
-            scene.SoundManager.ISoundEngine.SetListenerPosition(ListenerPosition.X, ListenerPosition.Y, 0f, 0,0, 1);
+            
             scene.SoundManager.ISoundEngine.SetListenerPosition(ListenerPosition.X, ListenerPosition.Y, 0f, 0, 0, 1);
 
             previousMouseState = currentMouseState;
