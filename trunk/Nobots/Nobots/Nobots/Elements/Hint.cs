ce3cf31d9f7d087bd4aefe77a02376fded707c6b
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@83858205-bffe-70c7-b075-45dc3756bde1>
2012-05-10 21:40:18 +0000

    Hint fades in and out according to the distance from the Camera.

--- trunk/Nobots/Nobots/Nobots/Elements/Hint.cs
+++ trunk/Nobots/Nobots/Nobots/Elements/Hint.cs
@@ -94,16 +94,29 @@ namespace Nobots.Elements
             arrow = Game.Content.Load<Texture2D>("hintarrow");
         }
 
+        float alpha = 0;
         public override void Draw(GameTime gameTime)
         {
-            //scene.SpriteBatch.Draw(blank, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - new Vector2((width + margin) / 2, height + margin / 2), null, Color.White, 0, Vector2.Zero, new Vector2(width, height), SpriteEffects.None, 0);
-            scene.SpriteBatch.Draw(arrow, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position), null, Color.White, 0, new Vector2(arrow.Width, arrow.Height), 1, SpriteEffects.None, 0);
+            if (scene.Camera.Target != null)
+            {
+                if (Vector2.DistanceSquared(scene.Camera.Target.Position, Position) < 50)
+                    alpha += alpha >= 1 ? 0 : (float)gameTime.ElapsedGameTime.TotalSeconds;
+                else
+                    alpha -= alpha <= 0 ? 0 : (float)gameTime.ElapsedGameTime.TotalSeconds;
+               // Console.WriteLine(alpha);
+                if (alpha > 0)
+                { 
+                    //scene.SpriteBatch.Draw(blank, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - new Vector2((width + margin) / 2, height + margin / 2), null, Color.White, 0, Vector2.Zero, new Vector2(width, height), SpriteEffects.None, 0);
+                    scene.SpriteBatch.Draw(arrow, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position), null, Color.White * alpha, 0, new Vector2(arrow.Width, arrow.Height), 1, SpriteEffects.None, 0);
 
-            scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) + Vector2.UnitX, Color.Black, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
-            scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - Vector2.UnitX, Color.Black, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
-            scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) + Vector2.UnitY, Color.Black, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
-            scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - Vector2.UnitY, Color.Black, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
-            scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position), Color.Yellow, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                    scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) + Vector2.UnitX, Color.Black * alpha, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                    scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - Vector2.UnitX, Color.Black * alpha, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                    scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) + Vector2.UnitY, Color.Black * alpha, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                    scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - Vector2.UnitY, Color.Black * alpha, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                    scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position) - Vector2.UnitY, Color.Black * alpha, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                    scene.SpriteBatch.DrawString(hintfont, Text, scene.Camera.Scale * Conversion.ToDisplay(position - scene.Camera.Position), Color.Yellow * alpha, 0, new Vector2(width / 2, height + arrow.Height), 1, SpriteEffects.None, 0);
+                }
+            }
         }
 
         protected override void Dispose(bool disposing)
